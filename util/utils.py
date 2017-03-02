fca958a3fb0a39413af5a78bf0a13d1048a2e773
Mihail Eric <mihail911@gmail.com>
2017-03-01 17:20:00 -0800

    utils comments

--- util/utils.py
+++ util/utils.py
@@ -1,12 +1,12 @@
-"""
-Basic utility for colored printing
-"""
-
 class Color:
+   """
+   Basic utility for colored printing
+   """
    GREEN = '\033[92m'
    YELLOW = '\033[93m'
    RED = '\033[91m'
    END = '\033[0m'
+
 
 def color_str(str, color=Color.GREEN):
    return color + str + Color.END
