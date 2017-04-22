efc028cd5fbd8c62df635c4f1b067a21d1b124d2
Mihail Eric <mihail911@gmail.com>
2017-04-21 21:47:53 -0700

    finished code for extracting annotated examples and formatting into dict that we can easily query to make examples for training model

--- util/parse_annotations.py
+++ util/parse_annotations.py
@@ -1,47 +1,109 @@
 import csv
 import json
+import re
 
-titles = ["P1_HAND", "P2_HAND", "P1_LOC", "P2_LOC",
-                "P1_KNOW", "P2_KNOW", "P1_NEED", "P2_NEED", "CARD_LOC",
-                "POINTER"]
+columns = ["P1_HAND", "P2_HAND", "P1_LOC", "P2_LOC",
+                "P1_KNOW", "P2_KNOW", "P1_NEED", "P2_NEED", "P1_ABLE", "P2_ABLE",
+                "CARD_LOC", "POINTER"]
 
 
-def parse_entry(entry):
+def categorize_label(speaker, pointer):
     """
-    Parse individual entry contained in a column, assumed to be colon separated
+    Categorize output of pointer (P1:720) into bins to format for training model
+    Right now I bin as follows: (SPEAKER, ADDRESSEE, NONE) -> (0, 1, 2)
+    Returns integer from bin above
+    :param ex:
+    :return:
+    """
+    if pointer == "NONE":
+        return 2
+    # Speaker performs action
+    if speaker in pointer:
+        return 0
+    # Addressee performs action
+    else:
+        return 1
+
+
+def extract_card(entry):
+    """
+    Extract card from entry. Expects pattern of form NEED("ax")
     :param entry:
     :return:
     """
-    elems = entry.split(";")
-    return elems
+    pattern = r"EXISTS\((.*)\)"
+    m = re.search(pattern, entry)
+    return m.group(1)
 
 
-def parse_file(file_path):
+def parse_line(line):
     """
-    Parse given file path
+    Parse line, represented as list
+    :param entry:
+    :return:
+    """
+    ex = {}
+    speaker = line[0]
+    # Remember to put speaker in example so we know how to categorize output later
+    if "1" in speaker:
+        ex["SPEAKER"] = "P1"
+    else:
+        ex["SPEAKER"] = "P2"
+
+    for col, val in zip(columns, line[4:]):
+        # Convert cards in hand into list
+        if col.endswith("HAND"):
+            hand = val.split(";")
+            ex[col] = hand
+        elif col.endswith("KNOW") or col.endswith("NEED"):
+            entries = val.split(";")
+            entries = [e.strip() for e in entries]
+            ex[col] = entries
+        elif col == "POINTER":
+            label = categorize_label(ex["SPEAKER"], val)
+            ex[col] = label
+        else:
+            ex[col] = val
+
+    # Extract card of interest --> Right now convention is that first
+    # entry in speaker "KNOW" list is the card of interest
+    if ex["SPEAKER"] == "P1":
+        coi = extract_card(ex["P1_KNOW"][0])
+    else:
+        coi = extract_card(ex["P2_KNOW"][0])
+    ex["COI"] = coi
+
+    return ex
+
+
+def parse_annotation_file(file_path):
+    """
+    Parse given annotation file path
     :param file_path:
     :return:
     """
     utterances = []
     with open(file_path) as f:
         reader = csv.reader(f, delimiter=",")
-        # Should have 10 columns
+        # Should have 14 columns
         for line in reader:
-            print line, len(line)
-            # Found utterance of interest
             if line[-1] != "":
-                utterance = {}
-                relevant = line[4:]
-                for title, entry in zip(titles, relevant):
-                    elems = parse_entry(entry)
-                    utterance[title] = elems
+                print "Good line: ", line[4:]
+                ex = parse_line(line)
+                print "Ex: ", ex
+                print "\n"
+                utterances.append(ex)
 
-                utterances.append(utterance)
-
-                print "Utterance: ", utterance
     return utterances
 
 
 if __name__ == "__main__":
-    annotations_file = "/Users/mihaileric/Documents/Research/cards/wild-card/data/annotated/cards_0000001_annotated.csv"
-    utterance = parse_file(annotations_file)
+    annotation_file_1 = "/Users/mihaileric/Documents/Research/cards/wild-card/data/annotated/cards_0000031_annotated.csv"
+    utterance = parse_annotation_file(annotation_file_1)
+
+    annotation_file_2 = "/Users/mihaileric/Documents/Research/cards/wild-card/data/annotated/cards_0000001_annotated.csv"
+    utterance = parse_annotation_file(annotation_file_2)
+
+    # annotation_file_3 = "/Users/mihaileric/Documents/Research/cards/wild-card/data/annotated/cards_0000031_annotated.csv"
+    # utterance = parse_annotation_file(annotation_file_3)
+
